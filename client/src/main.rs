c4bc56849bdc02a2f991155aded02c4d0c3118ba
Felix <51097237+f321x@users.noreply.github.com>
2024-07-11 19:14:34 +0000

    format all source files

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,70 +6,70 @@ use std::env;
 use anyhow::anyhow;
 use cashu_escrow_common as common;
 use common::cli::get_user_input;
+use common::nostr::NostrClient;
+use common::TradeContract;
 use dotenv::dotenv;
 use ecash::EcashWallet;
-use common::TradeContract;
 use escrow_client::{EscrowUser, Trader};
-use common::nostr::NostrClient;
 use nostr_sdk::prelude::*;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv().ok();
-    // parsing was hacked together last minute :)
-    // information would be communicated oob
-    let mut buyer_npub: String = env::var("BUYER_NPUB")?;
-    let mut seller_npub: String = env::var("SELLER_NPUB")?;
-    let coordinator_npub: String = env::var("ESCROW_NPUB")?;
-    let ecash_wallet = EcashWallet::new().await?;
-    let mut seller_ecash_pubkey: String = String::new();
-    let mut buyer_ecash_pubkey: String = String::new();
-    let nostr_client: NostrClient;
+	dotenv().ok();
+	// parsing was hacked together last minute :)
+	// information would be communicated oob
+	let mut buyer_npub: String = env::var("BUYER_NPUB")?;
+	let mut seller_npub: String = env::var("SELLER_NPUB")?;
+	let coordinator_npub: String = env::var("ESCROW_NPUB")?;
+	let ecash_wallet = EcashWallet::new().await?;
+	let mut seller_ecash_pubkey: String = String::new();
+	let mut buyer_ecash_pubkey: String = String::new();
+	let nostr_client: NostrClient;
 
-    let mode = match get_user_input("Select mode: (1) buyer, (2) seller: ")
-        .await?
-        .as_str()
-    {
-        "1" => {
-            nostr_client = NostrClient::new(&env::var("BUYER_NSEC")?).await?;
-            buyer_npub = nostr_client.get_npub()?;
-            //println!("Buyer npub: {}", &buyer_npub);
-            seller_ecash_pubkey = get_user_input("Enter seller's ecash pubkey: ").await?;
-            buyer_ecash_pubkey = ecash_wallet.trade_pubkey.clone();
-            String::from("buyer")
-        }
-        "2" => {
-            nostr_client = NostrClient::new(&env::var("SELLER_NSEC")?).await?;
-            seller_npub = nostr_client.get_npub()?;
-            //println!("Seller npub: {}", &seller_npub);
-            seller_ecash_pubkey = ecash_wallet.trade_pubkey.clone();
-            buyer_ecash_pubkey = get_user_input("Enter buyer's ecash pubkey: ").await?;
-            String::from("seller")
-        }
-        _ => {
-            panic!("Wrong trading mode selected. Select either (1) buyer or (2) seller");
-        }
-    };
+	let mode = match get_user_input("Select mode: (1) buyer, (2) seller: ")
+		.await?
+		.as_str()
+	{
+		"1" => {
+			nostr_client = NostrClient::new(&env::var("BUYER_NSEC")?).await?;
+			buyer_npub = nostr_client.get_npub()?;
+			//println!("Buyer npub: {}", &buyer_npub);
+			seller_ecash_pubkey = get_user_input("Enter seller's ecash pubkey: ").await?;
+			buyer_ecash_pubkey = ecash_wallet.trade_pubkey.clone();
+			String::from("buyer")
+		}
+		"2" => {
+			nostr_client = NostrClient::new(&env::var("SELLER_NSEC")?).await?;
+			seller_npub = nostr_client.get_npub()?;
+			//println!("Seller npub: {}", &seller_npub);
+			seller_ecash_pubkey = ecash_wallet.trade_pubkey.clone();
+			buyer_ecash_pubkey = get_user_input("Enter buyer's ecash pubkey: ").await?;
+			String::from("seller")
+		}
+		_ => {
+			panic!("Wrong trading mode selected. Select either (1) buyer or (2) seller");
+		}
+	};
 
-    let contract = TradeContract {
-        trade_beginning_ts: 1718975980,
-        trade_description: "Purchase of one Watermelon for 5000 satoshi. 3 days delivery to ..."
-            .to_string(),
-        trade_mint_url: "https://mint.minibits.cash/Bitcoin".to_string(),
-        trade_amount_sat: 5000,
-        npub_seller: seller_npub,
-        npub_buyer: buyer_npub,
-        time_limit: 3 * 24 * 60 * 60,
-        seller_ecash_public_key: seller_ecash_pubkey,
-        buyer_ecash_public_key: buyer_ecash_pubkey,
-    };
+	let contract = TradeContract {
+		trade_beginning_ts: 1718975980,
+		trade_description: "Purchase of one Watermelon for 5000 satoshi. 3 days delivery to ..."
+			.to_string(),
+		trade_mint_url: "https://mint.minibits.cash/Bitcoin".to_string(),
+		trade_amount_sat: 5000,
+		npub_seller: seller_npub,
+		npub_buyer: buyer_npub,
+		time_limit: 3 * 24 * 60 * 60,
+		seller_ecash_public_key: seller_ecash_pubkey,
+		buyer_ecash_public_key: buyer_ecash_pubkey,
+	};
 
-    let escrow_user =
-        EscrowUser::new(contract, ecash_wallet, nostr_client, coordinator_npub).await?;
+	let escrow_user =
+		EscrowUser::new(contract, ecash_wallet, nostr_client, coordinator_npub).await?;
 
-    match mode.as_str() {
-        "buyer" => Trader::Buyer(escrow_user).init_trade().await,
-        "seller" => Trader::Seller(escrow_user).init_trade().await,
-        _ => return Err(anyhow!("Invalid mode")),
-    }
+	match mode.as_str() {
+		"buyer" => Trader::Buyer(escrow_user).init_trade().await,
+		"seller" => Trader::Seller(escrow_user).init_trade().await,
+		_ => return Err(anyhow!("Invalid mode")),
+	}
 }
--- common/src/nostr/mod.rs
+++ common/src/nostr/mod.rs
@@ -1,96 +1,96 @@
 use crate::TradeContract;
-use nostr_sdk as ndk;
 use ndk::prelude::*;
+use nostr_sdk as ndk;
 use serde::Serialize;
 
 pub struct NostrClient {
-    keypair: Keys,
-    pub client: Client,
+	keypair: Keys,
+	pub client: Client,
 }
 
 #[derive(Serialize)]
 struct PubkeyMessage {
-    escrow_coordinator_pubkey: String,
-    trade_id_hex: String,
+	escrow_coordinator_pubkey: String,
+	trade_id_hex: String,
 }
 
 impl NostrClient {
-    pub async fn new(nsec: &String) -> anyhow::Result<Self> {
-        let keypair = Keys::parse(nsec)?;
+	pub async fn new(nsec: &String) -> anyhow::Result<Self> {
+		let keypair = Keys::parse(nsec)?;
 
-        let client = Client::new(&keypair);
+		let client = Client::new(&keypair);
 
-        client.add_relay("wss://relay.damus.io").await?;
-        client.add_relay("wss://relay.primal.net").await?;
-        client.add_relay("wss://relay.nostr.band").await?;
-        client
-            .add_relay("wss://ftp.halifax.rwth-aachen.de/nostr")
-            .await?;
-        client.add_relay("wss://nostr.mom").await?;
-        client.add_relay("wss://relay.nostrplebs.com").await?;
+		client.add_relay("wss://relay.damus.io").await?;
+		client.add_relay("wss://relay.primal.net").await?;
+		client.add_relay("wss://relay.nostr.band").await?;
+		client
+			.add_relay("wss://ftp.halifax.rwth-aachen.de/nostr")
+			.await?;
+		client.add_relay("wss://nostr.mom").await?;
+		client.add_relay("wss://relay.nostrplebs.com").await?;
 
-        // Connect to relays
-        client.connect().await;
-        Ok(Self { keypair, client })
-    }
+		// Connect to relays
+		client.connect().await;
+		Ok(Self { keypair, client })
+	}
 
-    pub fn get_npub(&self) -> anyhow::Result<String> {
-        Ok(self.keypair.public_key().to_bech32()?)
-    }
+	pub fn get_npub(&self) -> anyhow::Result<String> {
+		Ok(self.keypair.public_key().to_bech32()?)
+	}
 
-    pub async fn decrypt_msg(&self, msg: &String, sender_pk: &PublicKey) -> Option<String> {
-        let decrypted =
-            ndk::nostr::nips::nip04::decrypt(self.keypair.secret_key().unwrap(), sender_pk, msg);
-        if let Ok(decrypted) = decrypted {
-            return Some(decrypted);
-        }
-        None
-    }
+	pub async fn decrypt_msg(&self, msg: &String, sender_pk: &PublicKey) -> Option<String> {
+		let decrypted =
+			ndk::nostr::nips::nip04::decrypt(self.keypair.secret_key().unwrap(), sender_pk, msg);
+		if let Ok(decrypted) = decrypted {
+			return Some(decrypted);
+		}
+		None
+	}
 
-    pub async fn send_escrow_pubkeys(
-        &self,
-        receivers: (&String, &String),
-        id: &[u8; 32],
-        trade_pk: &String,
-    ) -> anyhow::Result<()> {
-        let message = serde_json::to_string(&PubkeyMessage {
-            escrow_coordinator_pubkey: trade_pk.clone(),
-            trade_id_hex: hex::encode(id),
-        })?;
-        self.client
-            .send_direct_msg(PublicKey::from_bech32(receivers.0)?, &message, None)
-            .await?;
-        self.client
-            .send_direct_msg(PublicKey::from_bech32(receivers.1)?, &message, None)
-            .await?;
-        Ok(())
-    }
+	pub async fn send_escrow_pubkeys(
+		&self,
+		receivers: (&String, &String),
+		id: &[u8; 32],
+		trade_pk: &String,
+	) -> anyhow::Result<()> {
+		let message = serde_json::to_string(&PubkeyMessage {
+			escrow_coordinator_pubkey: trade_pk.clone(),
+			trade_id_hex: hex::encode(id),
+		})?;
+		self.client
+			.send_direct_msg(PublicKey::from_bech32(receivers.0)?, &message, None)
+			.await?;
+		self.client
+			.send_direct_msg(PublicKey::from_bech32(receivers.1)?, &message, None)
+			.await?;
+		Ok(())
+	}
 
-    pub async fn send_escrow_contract(
-        &self,
-        contract: &TradeContract,
-        coordinator_pk_bech32: &String,
-    ) -> anyhow::Result<()> {
-        let message = serde_json::to_string(contract)?;
-        dbg!("sending contract to coordinator...");
-        self.client
-            .send_direct_msg(
-                PublicKey::from_bech32(coordinator_pk_bech32)?,
-                &message,
-                None,
-            )
-            .await?;
-        Ok(())
-    }
+	pub async fn send_escrow_contract(
+		&self,
+		contract: &TradeContract,
+		coordinator_pk_bech32: &String,
+	) -> anyhow::Result<()> {
+		let message = serde_json::to_string(contract)?;
+		dbg!("sending contract to coordinator...");
+		self.client
+			.send_direct_msg(
+				PublicKey::from_bech32(coordinator_pk_bech32)?,
+				&message,
+				None,
+			)
+			.await?;
+		Ok(())
+	}
 
-    pub async fn submit_trade_token_to_seller(
-        &self,
-        seller_npub: &String,
-        token: &String,
-    ) -> anyhow::Result<()> {
-        self.client
-            .send_direct_msg(PublicKey::from_bech32(seller_npub)?, token, None)
-            .await?;
-        Ok(())
-    }
+	pub async fn submit_trade_token_to_seller(
+		&self,
+		seller_npub: &String,
+		token: &String,
+	) -> anyhow::Result<()> {
+		self.client
+			.send_direct_msg(PublicKey::from_bech32(seller_npub)?, token, None)
+			.await?;
+		Ok(())
+	}
 }
