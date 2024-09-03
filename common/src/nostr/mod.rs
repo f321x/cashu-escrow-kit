94d2424cdfca80f33571c441077839ca56bbc9c4
rodant <antonio.rodriguez@posteo.de>
2024-09-03 10:01:48 +0200

    Handle receice errors from a subscription.

--- common/src/nostr/mod.rs
+++ common/src/nostr/mod.rs
@@ -3,12 +3,15 @@ use std::time::Duration;
 use crate::model::EscrowRegistration;
 use anyhow::anyhow;
 use nostr_sdk::prelude::*;
-use tokio::{sync::broadcast::Receiver, time::timeout};
+use tokio::{
+    sync::broadcast::{error::RecvError, Receiver},
+    time::timeout,
+};
 
 pub struct NostrClient {
     keys: Keys,
     pub client: Client,
-    _subscription_id: SubscriptionId,
+    subscription_id: SubscriptionId,
     notifications_receiver: Receiver<RelayPoolNotification>,
 }
 
@@ -28,18 +31,12 @@ impl NostrClient {
         // Connect to relays
         client.connect().await;
 
-        let message_filter = Filter::new()
-            .kind(Kind::GiftWrap)
-            .pubkey(keys.public_key())
-            .limit(0);
-
-        let _subscription_id = client.subscribe(vec![message_filter], None).await?.val;
-        let notifications_receiver = client.notifications();
+        let (_subscription_id, notifications_receiver) = init_subscription(&keys, &client).await?;
 
         Ok(Self {
             keys,
             client,
-            _subscription_id,
+            subscription_id: _subscription_id,
             notifications_receiver,
         })
     }
@@ -51,15 +48,25 @@ impl NostrClient {
     pub async fn receive_escrow_message(&mut self, timeout_secs: u64) -> anyhow::Result<String> {
         let loop_future = async {
             loop {
-                if let Ok(notification) = self.notifications_receiver.recv().await {
-                    if let RelayPoolNotification::Event { event, .. } = notification {
-                        let rumor = self.client.unwrap_gift_wrap(&event).await?.rumor;
-                        if rumor.kind == Kind::PrivateDirectMessage {
-                            break Ok(rumor.content) as anyhow::Result<String>;
+                match self.notifications_receiver.recv().await {
+                    Ok(notification) => {
+                        if let RelayPoolNotification::Event { event, .. } = notification {
+                            let rumor = self.client.unwrap_gift_wrap(&event).await?.rumor;
+                            if rumor.kind == Kind::PrivateDirectMessage {
+                                break Ok(rumor.content) as anyhow::Result<String>;
+                            }
                         }
                     }
+                    Err(RecvError::Closed) => {
+                        eprintln!("Relay pool closed subscription, restarting a new one...");
+                        self.client.unsubscribe(self.subscription_id.clone()).await;
+                        (self.subscription_id, self.notifications_receiver) =
+                            init_subscription(&self.keys, &self.client).await?;
+                    }
+                    Err(RecvError::Lagged(count)) => {
+                        dbg!("Lost {} events, proceeding after that...", count);
+                    }
                 }
-                //todo: in case of RecvErr::Close reset the subscription
             }
         };
         let result = match timeout(Duration::from_secs(timeout_secs), loop_future).await {
@@ -91,4 +98,17 @@ impl NostrClient {
             .await?;
         Ok(())
     }
+}
+
+async fn init_subscription(
+    keys: &Keys,
+    client: &Client,
+) -> Result<(SubscriptionId, Receiver<RelayPoolNotification>), anyhow::Error> {
+    let message_filter = Filter::new()
+        .kind(Kind::GiftWrap)
+        .pubkey(keys.public_key())
+        .limit(0);
+    let _subscription_id = client.subscribe(vec![message_filter], None).await?.val;
+    let notifications_receiver = client.notifications();
+    Ok((_subscription_id, notifications_receiver))
 }
