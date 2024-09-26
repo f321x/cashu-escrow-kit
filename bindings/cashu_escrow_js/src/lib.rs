37546ff6c39850d73d9bacd91749f18b786baf21
rodant <rodant68@gmail.com>
2024-09-26 15:28:27 +0200

    Ad binding for ecash wallet constructor.

--- bindings/cashu_escrow_js/src/lib.rs
+++ bindings/cashu_escrow_js/src/lib.rs
@@ -1,3 +1,4 @@
+use cashu_escrow_client::ecash::ClientEcashWallet;
 use cashu_escrow_common::nostr::NostrClient;
 use error::{into_err, Result};
 use nostr_sdk::prelude::*;
@@ -21,6 +22,20 @@ impl JsNostrClient {
     pub async fn new(key: &str) -> Result<JsNostrClient> {
         let keys = Keys::parse(key).map_err(into_err)?;
         let _inner = NostrClient::new(keys).await.map_err(into_err)?;
+        Ok(Self { _inner })
+    }
+}
+
+#[wasm_bindgen(js_name = ClientEcashWallet)]
+pub struct JsClientEcashWallet {
+    _inner: ClientEcashWallet,
+}
+
+#[wasm_bindgen(js_class = ClientEcashWallet)]
+impl JsClientEcashWallet {
+    #[wasm_bindgen(constructor)]
+    pub async fn new(url: &str) -> Result<JsClientEcashWallet> {
+        let _inner = ClientEcashWallet::new(url).await.map_err(into_err)?;
         Ok(Self { _inner })
     }
 }
--- bindings/cashu_escrow_js/web_client/index.js
+++ bindings/cashu_escrow_js/web_client/index.js
@@ -1,4 +1,8 @@
-import { NostrClient } from "cashu_escrow_js";
+import { ClientEcashWallet, NostrClient } from "cashu_escrow_js";
+
+
+const escrowWallet = await new ClientEcashWallet("http://localhost:3338");
+console.log("After ClientEcashWallet");
 
 const nostrClient = await new NostrClient("nsec182ul8zg2jlje6gtejs4pp4y4un674esq9qmrdxn2mewynkegahgqudmhvh");
 console.log("After NostrClient");
